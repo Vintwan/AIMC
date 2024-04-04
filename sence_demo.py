7ab246798327a9b29d2037ee17c328e393ea1594
Dannr <63134885+Dannr-hjy@users.noreply.github.com>
2024-04-04 18:24:21 +0800

    fix bugs

--- sence_demo.py
+++ sence_demo.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 from scenedetect import open_video, SceneManager, split_video_ffmpeg
 from scenedetect.detectors import ContentDetector
 from scenedetect.video_splitter import split_video_ffmpeg
--- splicing.py
+++ splicing.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import json
 import ollama
 from tqdm import tqdm 
