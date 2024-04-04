f15d1e375d200c506942f6893b0813ccb1f1a2cc
Dannr <63134885+Dannr-hjy@users.noreply.github.com>
2024-04-04 10:47:27 +0800

    add file

--- describe_rewrite.py
+++ describe_rewrite.py
@@ -1,9 +1,12 @@
 import json
+import ollama
+from tqdm import tqdm
 
 #调用模型优化
 def rec_image(model, images_path, prompt):
-    response = ollama.chat(model= model, messages=[
-        {"role": "system", "content": "You are ."},
+    with tqdm(total=1) as pbar:
+        response = ollama.chat(model= model, messages=[
+        {"role": "system", "content": "You're a writer who's good at extracting information"},
         {
             'role': 'user',
             'content': prompt
@@ -13,11 +16,11 @@ def rec_image(model, images_path, prompt):
         'images':images_path,
         'result':response['message']['content']
     }
-    #print(result)
+    print(result)
     return result 
 
 if __name__ == "__main__":
     with open('co_description.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
-    rec_image("gemma:7b", )
+    rec_image("gemma:7b",data, "Please summarize the information about the content of the picture in the sentence into a sentence of 15 words or less,including characters, plot, environment and historical background")
     
